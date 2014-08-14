892bdbd167fc7c1d2386ff2ad74777e03f68d11c
Paweł Suder <pawel@suder.info>
2014-08-14 12:39:15 +0200

    Fix Amazon tagging

--- scripts/openstack2amazon.sh
+++ scripts/openstack2amazon.sh
@@ -73,7 +73,16 @@ then
             sleep 30
 
             ec2-create-image -n ${image_uuid}-${hash_sum} -d "${image_name}-${image_uuid}-${hash_sum}" ${instance}
+
+            sleep 30
+
             __ami_id=$(ec2-describe-images | grep ${image_uuid}-${hash_sum} | awk '{print $2}')
+            until [ -n "${__ami_id}" ]
+            then
+                sleep 30
+                __ami_id=$(ec2-describe-images | grep ${image_uuid}-${hash_sum} | awk '{print $2}')
+            fi
+
             ec2-create-tags ${__ami_id} --tag source_cs=${SOURCE_CS} --tag source_uuid=${image_uuid} --tag Name=${image_name}-${image_uuid}-${hash_sum}
             echo "Registered"
 
