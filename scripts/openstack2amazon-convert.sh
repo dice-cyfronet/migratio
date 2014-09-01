4e5c19f013c1f4b526b3a8d6718af6e97b5b1610
Paweł Suder <pawel@suder.info>
2014-09-01 11:33:25 +0200

    Remove '

--- scripts/openstack2amazon-convert.sh
+++ scripts/openstack2amazon-convert.sh
@@ -25,7 +25,7 @@ command -v qemu-img > /dev/null 2>&1 || { echo "No 'qemu-img'" >&2; exit 1; }
 image_uuid=$1
 
 image_list=$(glance image-list) &>> ${__dir}/logs/o2a-c.log
-echo "$(date) [Result for 'glance image-list']: ${image_list}" &>> ${__dir}/logs/o2a-c.log
+echo "$(date) [Result for glance image-list]: ${image_list}" &>> ${__dir}/logs/o2a-c.log
 
 check_local=$(echo "${image_list}" | grep ${image_uuid} | wc -l) &>> ${__dir}/logs/o2a-c.log
 echo "$(date) [Result for looking for ${image_uuid in image_list]: ${check_local}" &>> ${__dir}/logs/o2a-c.log
@@ -38,7 +38,7 @@ then
     if [ ! -f /tmp/${image_uuid}.raw ]
     then
         __output=$(sudo qemu-img convert -f qcow2 -O raw /var/lib/glance/images/${image_uuid} /tmp/${image_uuid}.raw)
-        echo "$(date) [Result for 'qemu-img convert']: ${__output}" &>> ${__dir}/logs/o2a-c.log
+        echo "$(date) [Result for qemu-img convert]: ${__output}" &>> ${__dir}/logs/o2a-c.log
     fi
 
     echo "Image converted"
